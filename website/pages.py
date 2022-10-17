65b198cb6a49f83b241a31d4b5b4edb05281a2c8
Victor Caballero <ps4victor@gmail.com>
2022-10-17 16:49:57 -0500

    continuing building

--- website/pages.py
+++ website/pages.py
@@ -1,17 +1,13 @@
 from flask import Blueprint, render_template, request
 
-pages = Blueprint(__name__, "pages")
+pages = Blueprint('pages', __name__)
 
 @pages.route("/Home")
 def home():
-    return render_template("home.HTML", name="User")
+    return render_template("home.HTML")
 
 @pages.route("/Profile")#In order to use must url must be /Home/profile?name=EnterNameHere
 def profile():
     args = request.args
     name = args.get('name')
     return render_template("profile.HTML", name=name)
-
-@pages.route("/Register")
-def register():
-    return render_template("register.HTML")

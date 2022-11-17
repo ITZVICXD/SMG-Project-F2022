3c4a0bf24bfb0b61b45d5eedeed7b149521151af
Victor Caballero <ps4victor@gmail.com>
2022-11-16 20:31:34 -0600

    deployment 2?

--- website/__init__.py
+++ website/__init__.py
@@ -4,8 +4,6 @@ import os
 from flask_login import LoginManager
 from sqlalchemy.sql import func
 
-
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'blah blah blah'
@@ -13,20 +11,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'tes
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-db = SQLAlchemy(app)
+def create_app():
+    
 
-from .auth import auth
-from .pages import pages
 
-app.register_blueprint(pages, url_prefix='/')
-app.register_blueprint(auth, url_prefix='/')
+    db = SQLAlchemy(app)
 
-from .models import User
+    from .auth import auth
+    from .pages import pages
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+    app.register_blueprint(pages, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    return app
